58b12060ba421cc62f9841d1ebb602acd445cff0
uyom88 <117414745+uyom88@users.noreply.github.com>
2022-11-11 12:42:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,33 @@ $(function () {
     $("html, body").stop().animate({ scrollTop: tt });
   });
 });
+
+// q mn
+
+$(function () {
+  $(window).scroll(function () {
+    var windowTop = $(window).scrollTop() + 300;
+    $("#q_mn")
+      .stop()
+      .animate({ top: windowTop + "px" }, 500);
+    //$("#q_mn").stop().animate({속성:"속성값", 속성:"속성값" },1000);
+  });
+});
+
+$(".skill-per").each(function () {
+  var $this = $(this);
+  var per = $this.attr("per");
+  $this.css("width", per + "%");
+  $({ animatedValue: 0 }).animate(
+    { animatedValue: per },
+    {
+      duration: 1000,
+      step: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+      complete: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+    }
+  );
+});
