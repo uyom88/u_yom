8b2e9806d2c49185e18b3b02a72ad2858a05be02
uyom88 <117414745+uyom88@users.noreply.github.com>
2022-12-29 11:01:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ mHtml.animate({ scrollTop: 0 });
 $(window).on("wheel", function (e) {
   if (mHtml.is(":animated")) return;
   if (e.originalEvent.deltaY > 0) {
-    if (page == 6) return;
+    if (page == 7) return;
     page++;
   } else if (e.originalEvent.deltaY < 0) {
     if (page == 1) return;
